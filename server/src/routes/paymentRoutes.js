19f122dae674b6d8dcd5bc6a5f0521d79e0d1879
Sunil Gurjar <Sunilgurjar54319@gmail.com>
2026-07-11 16:19:35 +0530

    Add payment verify route

--- server/src/routes/paymentRoutes.js
+++ server/src/routes/paymentRoutes.js
@@ -2,10 +2,15 @@ const express = require("express");
 const router = express.Router();
 
 const {
-  createOrder
+  createOrder,
+  verifyPayment
 } = require("../controllers/paymentController");
 
+
 router.post("/create", createOrder);
+
+router.post("/verify", verifyPayment);
+
 
 router.get("/test", (req, res) => {
   res.json({
@@ -13,5 +18,6 @@ router.get("/test", (req, res) => {
     message: "Payment API Working"
   });
 });
+
 
 module.exports = router;
