0850c3014a5c4249476bbd5c391d88fd9ed2454f
Sunil Gurjar <Sunilgurjar54319@gmail.com>
2026-07-16 13:08:48 +0530

    Remove customer homepage

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,53 +1,7 @@
 import { Link } from "react-router-dom";
 
 function Header() {
-  return (
-    <header
-      style={{
-        background: "#2563eb",
-        color: "#fff",
-        padding: "20px",
-        textAlign: "center",
-        borderRadius: "10px",
-        marginBottom: "20px",
-      }}
-    >
-      <h1>🖨️ AutoPrint Hub</h1>
-      <p>Upload PDF • Pay Online • Auto Print</p>
-
-      <div
-        style={{
-          marginTop: "15px",
-          display: "flex",
-          justifyContent: "center",
-          gap: "10px",
-          flexWrap: "wrap",
-        }}
-      >
-        <Link to="/register-shop">
-          <button
-            style={{
-              padding: "10px 20px",
-              cursor: "pointer",
-            }}
-          >
-            🏪 Shop Register
-          </button>
-        </Link>
-
-        <Link to="/login">
-          <button
-            style={{
-              padding: "10px 20px",
-              cursor: "pointer",
-            }}
-          >
-            🔐 Shop Login
-          </button>
-        </Link>
-      </div>
-    </header>
-  );
+  return null;    
 }
 
 export default Header;
